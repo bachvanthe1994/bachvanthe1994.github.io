4be7a1cbc947731263fe8bf88f0cddb7f5ce37a4
thebv <thebv@thebvs-MacBook-Pro.local>
2024-04-04 17:36:15 +0700

    fix bug

--- demo/viewPdf/script.js
+++ demo/viewPdf/script.js
@@ -2,14 +2,10 @@ function yourFunction() {
   var url_string = window.location.href;
   var url = new URL(url_string);
   var pdfUrl = url.searchParams.get("pdfUrl");
-  var width = url.searchParams.get("width");
-  var height = url.searchParams.get("height");
   pdfUrl = decodeURI(pdfUrl);
   console.log("pdfUrl", pdfUrl);
   let element = document.getElementById("pdf-iframe");
   element.src = pdfUrl;
-  element.style.width = width;
-  element.style.height = height;
 }
 
 window.addEventListener
