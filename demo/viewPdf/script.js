90ab6bd44e65137a0accbc10a72c32dfe307b412
thebv <thebv@thebvs-MacBook-Pro.local>
2024-04-05 09:26:53 +0700

    fix bug

--- demo/viewPdf/script.js
+++ demo/viewPdf/script.js
@@ -4,8 +4,56 @@ function yourFunction() {
   var pdfUrl = url.searchParams.get("pdfUrl");
   pdfUrl = decodeURI(pdfUrl);
   console.log("pdfUrl", pdfUrl);
-  let element = document.getElementById("pdf-iframe");
-  element.src = pdfUrl;
+  // let element = document.getElementById("pdf-iframe");
+  // element.src = pdfUrl;
+
+  // If absolute URL from the remote server is provided, configure the CORS
+  // header on that server.
+  var url =
+    "https://partner.vnticketonline.vn/Resource/PDF/Ticket/20240511/9Y5IHV/69052197/80/05B06623F961C6DB1173CAE690B6799D/1712281879";
+
+  // Loaded via <script> tag, create shortcut to access PDF.js exports.
+  var { pdfjsLib } = globalThis;
+
+  // The workerSrc property shall be specified.
+  pdfjsLib.GlobalWorkerOptions.workerSrc = "pdf.worker.mjs";
+
+  // Asynchronous download of PDF
+  var loadingTask = pdfjsLib.getDocument(url);
+  loadingTask.promise.then(
+    function (pdf) {
+      console.log("PDF loaded");
+
+      // Fetch the first page
+      var pageNumber = 1;
+      pdf.getPage(pageNumber).then(function (page) {
+        console.log("Page loaded");
+
+        var scale = 1.5;
+        var viewport = page.getViewport({ scale: scale });
+
+        // Prepare canvas using PDF page dimensions
+        var canvas = document.getElementById("the-canvas");
+        var context = canvas.getContext("2d");
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        var renderContext = {
+          canvasContext: context,
+          viewport: viewport,
+        };
+        var renderTask = page.render(renderContext);
+        renderTask.promise.then(function () {
+          console.log("Page rendered");
+        });
+      });
+    },
+    function (reason) {
+      // PDF loading error
+      console.error(reason);
+    }
+  );
 }
 
 window.addEventListener
